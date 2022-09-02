cab485f0195cd43db03290180bbd988fbb46e41f
aesquivel94 <alejandraesqarias@gmail.com>
2022-09-02 17:43:57 -0400

    Tier variable and basic structure for reading data

--- Basic_conf.R
+++ Basic_conf.R
@@ -3,6 +3,7 @@
 # September/2022
 #  On going ---- 
 
+
 # R options
 options(warn = -1, scipen = 999)
 
@@ -36,23 +37,53 @@ links <- links %>%
   dplyr::select(year, month, docs)
 
 
-links
 
 
 
 # =---- Testing phase -- 
-f <- links[1,3] %>% pull()
+
 ### Read spreadsheeds 
+# =---  Function reading_basic: 
 
-read_web_sheets <- function(){
-  probando <- googlesheets4::read_sheet(ss = f, col_names = FALSE) 
+
+
+  # https://www.kaggle.com/datasets/ampiiere/acnh-villager-popularity
   
-  probando %>% 
+  # Each villager belongs to a tier(1-6), and within each tier there are rankings. Each Tier has a different amount of rankings.
+  # As of 2022, March:
+  # The "Highest Popularity" tier contains 15 villagers
+  # The "Very Popular" tier contains 25 villagers
+  # The "Fairly Popular" tier contains 30 villagers
+  # The "Middle Ground" tier contains 60 villagers
+  # The "Less Popular" tier contains 120 villagers
+  # The "Least Popular" tier contains 163 villagers
+  
+
+
+  url_doc <- links[1,3] %>% pull()
+  
+  # It doesn't work into a function by now... working on it
+  # Maybe i can iterate using a loop using a 
+  # list or something like that. And after that joing with the other tables. 
+  # Reading spreadsheet --- 
+  table_month <- googlesheets4::read_sheet(ss = url_doc, col_names = FALSE) %>% 
     as.tibble() %>% 
-    set_names(c('villager', 'votes'))
-  
-  googlesheets4::sheet_names(ss = f)
+    set_names(c('villager', 'votes')) %>% 
+    arrange(desc(votes)) %>% 
+    # This line is gonna be temporal, but it works by now. 
+    mutate(tier = c(rep("Highest_Popularity", 15),
+                    rep("Very_Popular", 25),
+                    rep("Fairly_Popular", 30),
+                    rep("Middle_Ground", 60),
+                    rep("Less_Popular", 120),
+                    rep("Less_Popular", n() - (15+25+30+60+120))))
 
-return()}
+
+
+  tier_table <- dplyr::bind_cols(links[1,], table_month) %>% 
+    dplyr::select(-docs)
+  
+
+
 
    
